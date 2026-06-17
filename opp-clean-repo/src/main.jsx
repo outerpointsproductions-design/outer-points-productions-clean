cba7ce06010b9339c46e33ea3f3cd1591ee849d0
outerpointsproductions-design <outerpointsproductions@gmail.com>
2026-06-16 22:00:50 -0500

    Update main.jsx

--- opp-clean-repo/src/main.jsx
+++ opp-clean-repo/src/main.jsx
@@ -104,9 +104,7 @@ function App(){useEffect(()=>{if(window.location.pathname.includes('badger-ko-si
 
   <a
     className="btn"
-    href="YOUR_NEW_PUBLIC_PLAYER_LIST_LINK"
-    target="_blank"
-    rel="noopener noreferrer"
+    href="#player-list"
   >
     View Current Player List
   </a>
@@ -173,7 +171,7 @@ function App(){useEffect(()=>{if(window.location.pathname.includes('badger-ko-si
     "View Full Format & Rules" button above.
   </p>
 
-</div> 
+</div> <section id="player-list" className="playerListBox">  <h2>Current Badger KO Player List</h2>  <iframe    src="YOUR_PUBLISHED_GOOGLE_SHEET_LINK"    width="100%"    height="600"    style={{ border: "0", borderRadius: "16px" }}  ></iframe</section>
    <form className="signupForm" action="https://formsubmit.co/outerpointsproductions@gmail.com" method="POST">
       <input type="hidden" name="_subject" value="New Badger KO Player Signup" />
       <input type="hidden" name="_captcha" value="false" />
