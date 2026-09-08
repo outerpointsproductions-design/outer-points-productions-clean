81cd3bdce374f8885147935f78a319a98ee4352d
outerpointsproductions-design <outerpointsproductions@gmail.com>
2026-09-08 15:44:10 -0500

    Update main.jsx

--- opp-clean-repo/src/main.jsx
+++ opp-clean-repo/src/main.jsx
@@ -242,7 +242,7 @@ function App(){
  <div className="heroBg"><img src={img('booth.jpg')}/></div><div className="heroShade"/>
  <div
   className="heroContent"
-  style={{ transform: 'translateY(-60px)' }}
+  style={{ transform: 'translateY(-120px)' }}
 >
 <div style={{
   display: 'flex',
