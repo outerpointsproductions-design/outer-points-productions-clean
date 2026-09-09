fc05140a2505032d75c396afe95670d020d25703
outerpointsproductions-design <outerpointsproductions@gmail.com>
2026-09-09 09:39:30 -0500

    Update main.jsx

--- opp-clean-repo/src/main.jsx
+++ opp-clean-repo/src/main.jsx
@@ -391,15 +391,7 @@ function App(){
   }}
   style={{ cursor: "pointer" }}
 >
-       <img
-  src={img(p)}
-  alt={n}
-  style={{
-    width: '100%',
-    height: '220px',
-    objectFit: 'contain'
-  }}
-/>
+<img src={img(p)} alt={t} />
         <div>
           <h3>{t}</h3>
           <p>{d}</p>
